edc35254ce28c4244611023a33b7505e68a308b3
Lucas Brasil Silva <lucasbrasil396@gmail.com>
2023-11-25 17:13:00 -0300

    updated code v.2

--- rede_social/rede_social/settings.py
+++ rede_social/rede_social/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-20f140-&$72p13%xwqw!w43q$-wr6ksgu^8_+5yc3z!!k0h@71"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
